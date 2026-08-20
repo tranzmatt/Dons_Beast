1ecf5e4fd374f1bfa77630a51794ffd2e4b017dc
Dwilliestyle <dwilliestyle@gmail.comm>
2026-08-20 09:55:41 -0400

    edited espeak for battery warning

--- beast_utils/beast_utils/battery_monitor.py
+++ beast_utils/beast_utils/battery_monitor.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 import subprocess
+import threading
 
 try:
     import smbus
@@ -62,13 +63,28 @@ class BatteryMonitor(Node):
     def play_audio_warning(self, message):
         """Play an audible warning using espeak"""
         try:
-            subprocess.Popen(
-                ['espeak', message],
-                stdout=subprocess.DEVNULL,
+            espeak_proc = subprocess.Popen(
+                ['espeak', '--stdout', message],
+                stdout=subprocess.PIPE,
                 stderr=subprocess.DEVNULL
             )
+            aplay_proc = subprocess.Popen(
+                ['aplay', '-D', 'plughw:USBAudio,0'],
+                stdin=espeak_proc.stdout,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.PIPE
+            )
+            espeak_proc.stdout.close()  # let aplay receive SIGPIPE if espeak exits early
+
+            def _log_aplay_errors(proc, logger):
+                err = proc.communicate()[1]
+                if proc.returncode != 0:
+                    logger.error(f'aplay failed: {err.decode(errors="ignore")}')
+            threading.Thread(
+                target=_log_aplay_errors, args=(aplay_proc, self.get_logger()), daemon=True
+            ).start()
         except Exception as e:
-            self.get_logger().debug(f'Could not play audio warning: {e}')
+            self.get_logger().error(f'Could not play audio warning: {e}')
     
     def read_battery(self):
         """Read battery voltage from INA219 sensor"""
